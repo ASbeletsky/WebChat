2020a4a0f7c3408e9c94aec189ff2920cb057f89
unknown <beletsky.anton@beletsky-pc.dev.local>
2016-03-22 18:49:18 +0200

    Add User Managment class

---
+++ src/Business/WebChat.Business.Management/CustomerManagment.cs
@@ -0,0 +1,35 @@
+﻿namespace WebChat.Business.Management
+{
+    using Domain.Data.Managers;
+    using Services.Interfaces;
+    #region Using
+
+    using System.Threading.Tasks;
+    using WebChat.Business.Core;
+    using WebChat.Business.Core.User;
+    using WebChat.Domain.Core.Identity;
+
+    #endregion
+    public class CustomerManagement : UserManagement
+    {
+        public CustomerManagement(AppUserManager storageUserManager, IEntityConverter entityConverter) : base(storageUserManager, entityConverter)
+        {
+
+        }
+
+        public async Task<Customer> CreateCustomer(Customer customer)
+        {
+            customer = (Customer) await base.CreateUser(customer);
+            var addToRoleResult = await DomainUserManager.AddToRoleAsync(customer.Id, Roles.Customer);
+            if(!addToRoleResult.Succeeded)
+            {
+                var addToRoleErrors = GetErrorsByKey(addToRoleResult.Errors, "Add to role Errors");
+                throw new BusinessLogicException(addToRoleErrors);
+            }
+            else
+            {
+                return customer;
+            }          
+        }           
+    }
+}
---
+++ src/Business/WebChat.Business.Management/UserManagement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebChat.Business.Core;
+using WebChat.Business.Core.User;
+using WebChat.Domain.Core.Identity;
+using WebChat.Domain.Data.Managers;
+using WebChat.Services.Interfaces;
+
+namespace WebChat.Business.Management
+{
+    public abstract class UserManagement
+    {
+        private AppUserManager _domainUserManager;
+        private IEntityConverter _entityConverter;
+
+        protected AppUserManager DomainUserManager;
+        protected IEntityConverter EntityConverter;
+
+        [Ninject.Inject]
+        public UserManagement(AppUserManager domainUserManager, IEntityConverter entityConverter)
+        {
+            this._domainUserManager = domainUserManager;
+            this._entityConverter = entityConverter;
+        }
+
+        public async Task<User> CreateUser(User user)
+        {
+            var userModel = _entityConverter.Convert<User, UserModel>(user);
+            var result = await _domainUserManager.CreateAsync(userModel);
+            if (result.Succeeded)
+            {
+                return _entityConverter.Convert<UserModel, Customer>(userModel);                
+            }
+
+            var createErrors = GetErrorsByKey(result.Errors, "Create user errors");
+            throw new BusinessLogicException(createErrors);
+        }
+
+        protected Dictionary<TKey, TValue> GetErrorsByKey<TKey, TValue>(IEnumerable<TValue> errors, TKey key)
+        {
+            var errorDictionary = new Dictionary<TKey, TValue>();
+            foreach (var error in errors)
+            {
+                errorDictionary.Add(key, error);
+            }
+
+            return errorDictionary;
+        }
+    }
+}
